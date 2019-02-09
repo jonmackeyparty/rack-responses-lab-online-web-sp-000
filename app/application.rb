a37ca2d0541c9efd33036b34f77a0b3a8d60de57
Jon Mackey <jon@jonmackey.party>
2019-02-09 17:05:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.new 
-    if time.hour.to_i > 12 
+    if time.hour.to_i >= 12 
       resp.write "Good Afternoon!" 
     else 
       resp.write "Good Morning!"
