a5d599c8839f0373e4c2402353ff3ab1c7f48255
Jon Mackey <jon@jonmackey.party>
2019-02-09 17:02:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,13 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    
-    resp.write "Hands off my web browser!"
+    time = Time.new 
+    if time.hour.to_i < 12 
+      resp.write "Good Morning!" 
+    else 
+      resp.write "Good Afternoon!"
+    end    
+
     resp.finish
   end
 
